a5b4db8e89844c9e9abd009720c7208986c5e3bb
Letizia29 <letizia.29.lr@gmail.com>
2024-05-24 09:17:17 +0200

    immagini patterns nan

--- missing_values_analysis.R
+++ missing_values_analysis.R
@@ -4,17 +4,20 @@ library(VIM)
 new_data_hc = read.csv("new_data_hc.csv")
 new_data_pd = read.csv("new_data_pd.csv")
 
-# immagine di
-matrixplot(new_data_hc)
-matrixplot(new_data_pd)
-
-# immagine di
-barMiss(new_data_hc)
-barMiss(new_data_pd)
-
 # labels
-names = list("NP1PTOT", "NP1RTOT", "NP2TOT", "NP3TOT", "NP4TOT", "GENETICS", "FAMILIARITY", "ETHNICITY", "SEX", "AGE", "HEIGHT", "WEIGHT", "HAND", "PRIM_DIAG")
+names = c("NP1PTOT", "NP1RTOT", "NP2TOT", "NP3TOT", "NP4TOT", "GENETICS", "FAMILIARITY", "ETHNICITY", "SEX", "AGE", "HEIGHT", "WEIGHT", "HAND", "PRIM_DIAG")
 
+# missing values patterns
+matrixplot(new_data_hc, cex.axis=0.7)
+title('HC - missing values patterns')
+matrixplot(new_data_pd, cex.axis=0.7)
+title('PD - missing values patterns')
+
+# proportions of missings
+colnames(new_data_hc) = names
+
+barMiss(new_data_hc, pos=5)
+barMiss(new_data_pd, pos=14)
 
 # combinazioni di missing values
 aggr(new_data_hc, labels=names, cex.axis=0.8)
