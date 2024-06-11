646fb0517fff99e2b14aeb884f0dbceb6ebbc9f9
Letizia29 <letizia.29.lr@gmail.com>
2024-06-11 16:30:42 +0200

    immagini sistemate

--- missing_values_analysis.R
+++ missing_values_analysis.R
@@ -8,10 +8,15 @@ new_data_pd = read.csv("new_data_pd.csv")
 names = c("NP1PTOT", "NP1RTOT", "NP2TOT", "NP3TOT", "NP4TOT", "GENETICS", "FAMILIARITY", "ETHNICITY", "SEX", "AGE", "HEIGHT", "WEIGHT", "HAND", "PRIM_DIAG")
 
 # missing values patterns
-matrixplot(new_data_hc, cex.axis=0.7, ylab='Subjects')
+png(file = 'missing_patterns_HC.png', width=1250 , height=500, res=100)
+matrixplot(new_data_hc, labels=names, cex.axis=0.7, ylab='Subjects')
 title('HC - missing values patterns')
-matrixplot(new_data_pd, cex.axis=0.7, ylab='Subjects')
+dev.off()
+
+png(file = 'missing_patterns_PD.png', width=1250 , height=500, res=100)
+matrixplot(new_data_pd, labels=names, cex.axis=0.7, ylab='Subjects')
 title('PD - missing values patterns')
+dev.off()
 
 # proportions of missings
 colnames(new_data_hc) = names
@@ -20,6 +25,11 @@ barMiss(new_data_hc, pos=5)
 barMiss(new_data_pd, pos=14)
 
 # combinazioni di missing values
+png(file = 'missing_values_HC.png', width=1250 , height=500, res=100)
 aggr(new_data_hc, labels=names, cex.axis=0.8)
+dev.off()
+
+png(file = 'missing_values_PD.png', width=1250 , height=500, res=100)
 aggr(new_data_pd, labels=names, cex.axis=0.8)
+dev.off()
 
